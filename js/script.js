86d6da2b8e7db77186a822f0a22bfb27822b882d
Mark-dev30 <fronterremarco@gmail.com>
2022-12-13 16:29:52 +0100

    MILESTONE 2 finished

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ MILESTONE 1: Stampare su console, per ogni membro del team, le informazioni di n
 MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
 Una volta che avete terminato una milestone, fare un push e nel messaggio indicare che avete terminato la milesione numero X. */
 
-//MILESTONE 0
+//MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+
 //1 - Creo l'array di oggetti con le informazioni fornite
 
 let our_team =
@@ -41,10 +42,21 @@ let our_team =
     },
 ];
 
-//MILESTONE 1
-//2 - 
+//MILESTONE 1 Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//MILESTONE 2: Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+//2 - Creo un ciclo for per accedere ai singoli aggetti dell'array.
+//  2.1 - All'interno del ciclo for creo un altro ciclo for per accedere alle informazioni dell'oggetto.
+//  2.2 - Visualizzo a schermo le informazioni di ogni oggetto ciclato.
+
+
+let team = document.querySelector(".team");
 for (let i=0; i<our_team.length; i++ ){
     for(let key in our_team[i]){
-        console.log(our_team[i][key]);
+        team.innerHTML += (`<li>${our_team[i][key]}</li>`);
     }
 }
+
+
+
