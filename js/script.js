e45069b83596c77fbf3ed8f593d4c9ff1510ac4e
Mark-dev30 <fronterremarco@gmail.com>
2022-12-13 14:38:02 +0100

    MILESTONE 0 finished

--- js/script.js
+++ js/script.js
@@ -6,4 +6,36 @@ Una volta che avete terminato una milestone, fare un push e nel messaggio indica
 
 //1 - Creo l'array di oggetti con le informazioni fornite
 
-
+let our_team
+[
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    },
+];
